024568fb3a68345d51561a731e3ae9063163bb61
Sean Nakamura <seanenakamura@gmail.com>
2015-07-02 19:22:45 -1000

    connected functions with matrix

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -1,7 +1,7 @@
 /*  MatrixRotator(matrix)
  *
  *  @param matrix                        a multidimensional array containing the matrix
- *  
+ *
  *  @public property matrix              the matrix
  *
  *  @public method rotate(direction)     direction is either
@@ -13,15 +13,26 @@ var Direction = require("./Direction").Direction;
 
 function MatrixRotator(matrix){
   this.matrix = matrix;
-  
+  // console.log(matrix);
 };
 
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
 MatrixRotator.prototype.rotate = function(direction) {
-  // do work here
-
-  // must be a valid Direction, see Direction.js
-
-
+  var thisMatrix = this.matrix;
+  if(direction === Direction.CW){
+    clockwise(thisMatrix);
+  }
+  if(direction === Direction.CCW){
+    counterClockwise(thisMatrix);
+  }
 };
+
+function clockwise (matrix){
+  console.log('this is CW');
+  console.log(matrix);
+}
+
+function counterClockwise(matrix){
+  console.log('this is CCW');
+}
